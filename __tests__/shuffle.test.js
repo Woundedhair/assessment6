28fa590361f10813201b6567879ea4ed27e07144
Woundedhair <stephentomiwa54@gmail.com>
2024-01-12 18:31:57 -0500

    First commit

---
+++ __tests__/shuffle.test.js
@@ -0,0 +1,11 @@
+const shuffle = require("../src/shuffle");
+
+describe("shuffle should...", () => {
+  test("return an array", () => {
+    var shuffle = [];
+    //expect(shuffle.toHaveReturned(array))
+  })
+  test("check that all the same items are in the array", () => {
+    expect(shuffle.toContainEqual([]));
+  })
+});
